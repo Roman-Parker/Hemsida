78e70088fb2416fdd64455047f178898feb861d2
Roman <roman@pop-os.localdomain>
2021-10-28 21:38:02 +0200

    added analys and responsive mobileview

---
+++ javascript.js
@@ -0,0 +1,24 @@
+const navSlide = () =>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    burger.addEventListener('click', ()=> {
+        //öppnar och stänger menyn i mobilvyn
+        nav.classList.toggle('nav-active')
+
+        //animerar länkarna i mobilvyn
+        navLinks.forEach((link, index) => {
+        
+            if(link.style.animation){
+                link.style.animation =""
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+            }
+            console.log(index/7);
+        });
+    });
+    
+}
+navSlide();
