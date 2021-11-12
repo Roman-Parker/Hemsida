f4abf4bed5fa77bcdf53434809e5218cfe155e3b
Roman <roman@pop-os.localdomain>
2021-11-12 14:16:50 +0100

    småfix

--- assets/javascript.js
+++ assets/javascript.js
@@ -11,16 +11,17 @@ const navSlide = () =>{
         //öppnar och stänger menyn i mobilvyn
         nav.classList.toggle('nav-active')
 
-        //animerar länkarna i mobilvyn
+        //animerar länkarna i mobilvyn, index=antalet länkar i menyn
+        //gör det enkelt att ändra antalet länkar i menyn utan att ändra på animeringen
         navLinks.forEach((link, index) => {
-        
+            
+            //Kollar om menyn redan är på skärmen
             if(link.style.animation){
                 link.style.animation =""
             }
             else{
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
             }
-            console.log(index/7);
         });
         //Animarear burgaren
         burger.classList.toggle("toggle");
