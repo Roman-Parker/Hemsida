210bcaeac75bf9ab155b57c810ccc743d523e0ec
Roman <roman@pop-os.localdomain>
2021-11-12 14:05:07 +0100

    changed filestructure and som class names along with spelling errors

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,8 +1,12 @@
+/** funktion för att animera menyn i mobilvyn */
 const navSlide = () =>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    
+    /**Lista med alla länkar i menyn */
     const navLinks = document.querySelectorAll('.nav-links li');
     
+    //Väntar på att användaren ska klicka på "burgaren"
     burger.addEventListener('click', ()=> {
         //öppnar och stänger menyn i mobilvyn
         nav.classList.toggle('nav-active')
